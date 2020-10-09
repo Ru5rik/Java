6c3385cc54ae91d70d86a64a1a482ef794fbaefe
Ru5rik <61066315+Ru5rik@users.noreply.github.com>
2020-10-09 10:30:05 +0300

    Create Prak1_4var.java

---
+++ Prak1_4var.java
@@ -0,0 +1,25 @@
+public class Main
+{
+	public static void main(String[] args) {
+		//1
+		System.out.println("1");
+		int a = 8;
+		int b = 7;
+		int c = 2;
+		int d = 3;
+		int z = 5;
+		double Answer = ((a/c)*(b/d)-(a*b-c)/(c*d));
+		System.out.println(Answer);
+		Answer = Math.cos(Math.sin(1/z));
+		System.out.println(Answer);
+		//2
+		System.out.println("2");
+		int R = 10;
+		int r = 7;
+		Answer = 3.14 * ((Math.pow(R, 2)) - (Math.pow(r, 2)));
+	    System.out.println(Answer);
+	    //3
+	    System.out.println("3");
+	    
+	}
+}
