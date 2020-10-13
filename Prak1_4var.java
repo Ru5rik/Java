7c7c9d517a2b1474d1ae7c5f677bf6edfe0afc0f
Ru5rik <61066315+Ru5rik@users.noreply.github.com>
2020-10-13 13:18:29 +0300

    Update Prak1_4var.java

--- Prak1_4var.java
+++ Prak1_4var.java
@@ -1,35 +1,54 @@
-public class Main
+class Main
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		 
 		//1
 		System.out.println("__1__");
-		int a = 8;
-		int b = 7;
-		int c = 2;
-		int d = 3;
-		int z = 5;
-		double Answer = ((a/c)*(b/d)-(a*b-c)/(c*d));
-		System.out.println(Answer);
-		Answer = Math.cos(Math.sin(1/z));
-		System.out.println(Answer);
+		System.out.println("Enter A: ");
+		int a = in.nextInt();
+		System.out.println("Enter B: ");
+		int b = in.nextInt();
+		System.out.println("Enter C: ");
+		int c = in.nextInt();
+		System.out.println("Enter D: ");
+		int d = in.nextInt();
+		System.out.println("Enter Z: ");
+		a = in.nextInt();
+		System.out.println("(a/c)*(b/d)-(a*b-c)/(c*d) = " + ((a/c)*(b/d)-(a*b-c)/(c*d)));
+		System.out.println("cos(sin(1/z)) = " + Math.cos(Math.sin(1/a)));
+
 		//2
 		System.out.println("__2__");
-		int R = 10;
-		int r = 7;
-		Answer = 3.14 * ((Math.pow(R, 2)) - (Math.pow(r, 2)));
-	    System.out.println(Answer);
+		System.out.println("R > r. Enter R: ");
+		a = in.nextInt();
+		System.out.println("Enter r: ");
+		b = in.nextInt();
+		if (a < b)
+		{
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		System.out.println("S = " + 3.14 * ((Math.pow(a, 2)) - (Math.pow(b, 2))));
+		
 	    //3
-	    System.out.println("__3__");
-	    double year = 2021;
+		System.out.println("__3__");
+		System.out.println("Enter year: ");
+	    double year = in.nextDouble();
 	    if ((year / 4) % 1 == 0)
 	        System.out.println("True");
 	    else
-	        System.out.println("Flase");
+			System.out.println("Flase");
+			
 	    //4
-	    System.out.println("__4__");
-	    int x = 4;
-	    int y = 1;
-	    if (y >= (x - 3) && y >= -1 * (x - 5) && y <= x + 3 && y <= -1 * x + 11)
+		System.out.println("__4__");
+		System.out.println("Enter x: ");
+		a = in.nextInt();
+		System.out.println("Enter y: ");
+		b = in.nextInt();
+	    if (b >= (a - 3) && b >= -1 * (a - 5) && b <= a + 3 && b <= -1 * a + 11)
 	        System.out.println("True");
 	    else
 	        System.out.println("False");
