0650bc8d9c9ee4365e18e30f84f1017a9f6552d4
Ru5rik <61066315+Ru5rik@users.noreply.github.com>
2020-10-13 13:41:05 +0300

    Update Prak1_4var.java

--- Prak1_4var.java
+++ Prak1_4var.java
@@ -7,47 +7,47 @@ class Main
 		//1
 		System.out.println("__1__");
 		System.out.println("Enter A: ");
-		int a = in.nextInt();
+		double a = in.nextDouble();
 		System.out.println("Enter B: ");
-		int b = in.nextInt();
+		double b = in.nextDouble();
 		System.out.println("Enter C: ");
-		int c = in.nextInt();
+		double c = in.nextDouble();
 		System.out.println("Enter D: ");
-		int d = in.nextInt();
+		double d = in.nextDouble();
+		System.out.println("(a/c)*(b/d)-(a*b-c)/(c*d) = " + ((a/c)*(b/d)-(a*b-c)/(c*d)));
 		System.out.println("Enter Z: ");
 		a = in.nextInt();
-		System.out.println("(a/c)*(b/d)-(a*b-c)/(c*d) = " + ((a/c)*(b/d)-(a*b-c)/(c*d)));
-		System.out.println("cos(sin(1/z)) = " + Math.cos(Math.sin(1/a)));
+		System.out.println("cos(sin(1/z)) = " + Math.pow(Math.cos(Math.sin(1/a)), 2));
 
-		//2
+		// //2
 		System.out.println("__2__");
 		System.out.println("R > r. Enter R: ");
-		a = in.nextInt();
+		a = in.nextDouble();
 		System.out.println("Enter r: ");
-		b = in.nextInt();
+		b = in.nextDouble();
 		if (a < b)
 		{
-			int temp = a;
+			double temp = a;
 			a = b;
 			b = temp;
 		}
 		System.out.println("S = " + 3.14 * ((Math.pow(a, 2)) - (Math.pow(b, 2))));
 		
-	    //3
+	    // //3
 		System.out.println("__3__");
 		System.out.println("Enter year: ");
-	    double year = in.nextDouble();
-	    if ((year / 4) % 1 == 0)
+	    a = in.nextDouble();
+	    if ((a / 4) % 1 == 0)
 	        System.out.println("True");
 	    else
 			System.out.println("Flase");
 			
-	    //4
+	    // //4
 		System.out.println("__4__");
 		System.out.println("Enter x: ");
-		a = in.nextInt();
+		a = in.nextDouble();
 		System.out.println("Enter y: ");
-		b = in.nextInt();
+		b = in.nextDouble();
 	    if (b >= (a - 3) && b >= -1 * (a - 5) && b <= a + 3 && b <= -1 * a + 11)
 	        System.out.println("True");
 	    else
